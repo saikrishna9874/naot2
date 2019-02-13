ba635260d5a594d619e090e406213d9361318830
saikrishna9874 <saikrishna9874@gmail.com>
2019-02-13 16:17:53 -0500

    Updates

---
+++ precache-manifest.5a313a6693e5aa6f186751278c51af9c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "d1a7da519f4553fd1ddd",
+    "url": "/static/js/main.d1a7da51.chunk.js"
+  },
+  {
+    "revision": "3df9e11a0f5ee2bad9d4",
+    "url": "/static/js/2.3df9e11a.chunk.js"
+  },
+  {
+    "revision": "d1a7da519f4553fd1ddd",
+    "url": "/static/css/main.82e154dc.chunk.css"
+  },
+  {
+    "revision": "3df9e11a0f5ee2bad9d4",
+    "url": "/static/css/2.9f3f3862.chunk.css"
+  },
+  {
+    "revision": "66c09052a8ba853f1344bac266ea2466",
+    "url": "/index.html"
+  }
+];
